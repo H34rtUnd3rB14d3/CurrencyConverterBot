db4edb4aa71e4f05bb29c044e32b2517d18b28b3
H34rtUnd3rB14d3 <k.maksim.e@yandex.ru>
2021-10-05 20:29:58 +0300

    Complete bot

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import telebot
 from config import TOKEN, exchanger
-from extensions import Converter, APIExceptions
+from extensions import Converter, APIExceptions, UserDB
 
 bot = telebot.TeleBot(TOKEN)
+db = UserDB()
 
 def_conv = {
     1: ("USD", "RUB")
@@ -12,8 +13,9 @@ def_conv = {
 @bot.message_handler(commands=["start", "help"])
 def send_welcome(message: telebot.types.Message):
     text = f"Welcome, {message.chat.username}\nTo get started, enter the command following way\n"
-    text += "<currency name> <converted currency> <amount>\n"
-    text += "To get list of available currencies type /values"
+    text += "/values To get list of available currencies\n"
+    text += "/set For setting converted currencies with buttons\n"
+    text += "Default conversion is USD -> RUB"
     bot.send_message(message.chat.id, text)
 
 
@@ -43,18 +45,21 @@ def setter(message: telebot.types.Message):
 @bot.callback_query_handler(func=lambda call: True)
 def handle_query(call):
     cur_pos, cur = call.data.split()
+    user_id = call.message.chat.id
     if cur_pos == "cur1":
-        def_conv[1] = (cur, def_conv[1][1])
+        db.change_from(user_id, cur)
     if cur_pos == "cur2":
-        def_conv[1] = (def_conv[1][0], cur)
+        db.change_to(user_id, cur)
+
+    pair = db.get_pair(user_id)
     bot.answer_callback_query(callback_query_id=call.id, show_alert=True,
-                              text=f"Now convert from {def_conv[1][0]} to {def_conv[1][1]}")
+                              text=f"Now convert from {pair[0]} to {pair[1]}")
 
 
 @bot.message_handler(content_types=["text"])
 def converter(message: telebot.types.Message):
-    values = [*def_conv[1], message.text.strip()]
-    values = list(map(str.lower, values))
+    pair = db.get_pair(message.chat.id)
+    values = [*pair, message.text.strip()]
     try:
         result = Converter.get_price(values)
     except APIExceptions as e:
@@ -62,7 +67,7 @@ def converter(message: telebot.types.Message):
     except Exception as e:
         bot.reply_to(message, e)
     else:
-        text = f"{values[2]} {exchanger[values[0]]} = {result} {exchanger[values[1]]}"
+        text = f"{values[2]} {values[0]} = {result} {values[1]}"
         bot.send_message(message.chat.id, text)
 
 
--- extensions.py
+++ extensions.py
@@ -1,10 +1,30 @@
 import requests
 import json
 from config import API_KEY
+from collections import defaultdict
 
 
 class APIExceptions(Exception):
     pass
+
+class UserInfo:
+    def __init__(self):
+        self.from_cur = "USD"
+        self.to_cur = "RUB"
+
+class UserDB:
+    def __init__(self):
+        self.db = defaultdict(UserInfo)
+
+    def change_from(self, user_id, value):
+        self.db[user_id].from_cur = value
+
+    def change_to(self, user_id, value):
+        self.db[user_id].to_cur = value
+
+    def get_pair(self, user_id):
+        return self.db[user_id].from_cur, self.db[user_id].to_cur
+
 
 
 class Converter:
@@ -15,6 +35,7 @@ class Converter:
         quote, base, amount = values
         quote_ticker = quote
         base_ticker = base
+        print(quote_ticker, base_ticker)
         try:
             amount = float(amount)
         except ValueError:
